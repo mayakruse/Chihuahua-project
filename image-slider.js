0e28f954ff0fcaf2b7f92de181a195b760e7707b
Maya Kruse <mayakruse@MacBook-Pro.local>
2020-07-27 19:01:07 -0400

    added image-slider project

---
+++ image-slider.js
@@ -0,0 +1,7 @@
+const image = [
+  "https://www.perfectdogbreeds.com/wp-content/uploads/2019/12/Chihuahua-and-Toys.jpg",
+  "https://www.yourpurebredpuppy.com/dogbreeds/photos2-C/chihuahua-05.jpg",
+  "https://www.k9web.com/wp-content/uploads/2019/05/Deer-Head-Chihuahua-1-780x520.jpg",
+  "https://thehappypuppysite.com/wp-content/uploads/2019/02/Black-And-White-Chihuahua-long.jpg",
+  "https://i.pinimg.com/originals/03/24/ba/0324ba0cd05c5accaa313da4398a71da.jpg",
+];
