76296d1dd2910b5220989e8fd375404658e9e570
Maya Kruse <mayakruse@MacBook-Pro.local>
2020-07-29 16:18:44 -0400

    updated buttons and added left butter js

--- image-slider.js
+++ image-slider.js
@@ -3,7 +3,7 @@ let puppies = [
   "https://www.yourpurebredpuppy.com/dogbreeds/photos2-C/chihuahua-05.jpg",
   "https://www.k9web.com/wp-content/uploads/2019/05/Deer-Head-Chihuahua-1-780x520.jpg",
   "https://thehappypuppysite.com/wp-content/uploads/2019/02/Black-And-White-Chihuahua-long.jpg",
-  "https://i.pinimg.com/originals/03/24/ba/0324ba0cd05c5accaa313da4398a71da.jpg",
+  "https://www.perfectdogbreeds.com/wp-content/uploads/2019/12/Chihuahua.jpg",
 ];
 
 let rightButton = document.querySelector(".right");
@@ -20,19 +20,11 @@ rightButton.addEventListener("click", function () {
   img.src = puppies[i];
 });
 
-/*let changeRight = function () {
-  let img = document.getElementById("myImg");
-  if (img.src === puppies[0]) {
-    img.src === puppies[1];
-  } else if (img.src === puppies[1]) {
-    img.src === puppies[2];
-  } else if (img.src === puppies[2]) {
-    img.src === puppies[3];
-  } else if (img.src === puppies[3]) {
-    img.src === puppies[4];
+leftButton.addEventListener("click", function () {
+  if (i === 0) {
+    i += 4;
   } else {
-    img.src === puppies[0];
+    --i;
   }
-};
-
-*/
+  img.src = puppies[i];
+});
